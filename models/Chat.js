00638787299e3a6402aa935127de060859292781
Do_Hoang_Long <hoanglong180903@gmail.com>
2025-03-04 16:56:26 +0700

    Coding 4/3/2025

--- models/Chat.js
+++ models/Chat.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const chatSchema  = new mongoose.Schema({
-    chatId: { type: String, required: true, unique: true },
-    messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "message" }],
-    lastMsg: {type : String},
-    lastMsgTime: {type : Number}
+    // chatId: { type: String, required: true, unique: true },
+    // messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "message" }],
+    // lastMsg: {type : String},
+    // lastMsgTime: {type : Number}
+
+    messageImage : {type : String},
+    messageText: {type : String},
+    senderId : {type : String},
+    timestamp: {type : Number}
 });
 
 module.exports = mongoose.model('chat', chatSchema);
