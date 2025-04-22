5ba03b279a968bd89ac4c8339b570dfe7a0d357e
Do_Hoang_Long <hoanglong180903@gmail.com>
2025-04-22 14:44:45 +0700

    Coding 22/4/2025

--- src/routes/api/AuthApi.js
+++ src/routes/api/AuthApi.js
@@ -51,9 +51,78 @@ router.get("/generateOTP", async (req, res) => {
         let mailOptions = {
             from    : user_name,
             to      : email_to,
-            subject : 'Confirm OTP ✔',
+            subject : 'OTP Verification ✔',
             text    : 'Hello world?',
-            html    : `<h4>Confirm OTP</h4> // add your HTML code here.`,
+            html    : `<!DOCTYPE html>
+                          <html lang="en">
+                              <head>
+                                  <meta charset="UTF-8">
+                                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                  <title>Verification OTP</title>
+                                  <style>
+                                      body {
+                                          font-family: Arial, sans-serif;
+                                          background-color: #f8f8f8;
+                                          margin: 0;
+                                          padding: 0;
+                                      }
+                                      .container {
+                                          max-width: 600px;
+                                          margin: 0 auto;
+                                          background-color: #ffffff;
+                                          border-radius: 5px;
+                                          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+                                      }
+                                      .header {
+                                          background-color: #0073e6;
+                                          color: #ffffff;
+                                          padding: 20px;
+                                          text-align: center;
+                                          border-top-left-radius: 5px;
+                                          border-top-right-radius: 5px;
+                                      }
+                                      .content {
+                                          padding: 20px;
+                                          text-align: center;
+                                      }
+                                      .otp {
+                                          font-size: 32px;
+                                          font-weight: bold;
+                                          color: #0073e6;
+                                      }
+                                      .footer {
+                                          border: 1px dashed #cccccc;
+                                          border-width: 2px 0;
+                                          padding: 20px;
+                                          text-align: center;
+                                      }
+                                      .footer a {
+                                          color: #0073e6;
+                                      }
+                                      .footer a:hover {
+                                          text-decoration: underline;
+                                      }
+                                  </style>
+                              </head>
+                              <body>
+                                  <div class="container">
+                                      <div class="header">
+                                          <h1>Verification</h1>
+                                          <p style="font-size: 14px;color: #ffffff;">
+                                              You received this email because you requested an OTP.
+                                          </p>
+                                      </div>
+                                      <div class="content">
+                                          <p>Your One-Time Password (OTP) is:</p>
+                                          <p class="otp">${req.app.locals.OTP}</p>
+                                      </div>
+                                      <div class="footer">
+                                          <p>For more information, visit our GitHub repository:</p>
+                                          <p><a href="https://github.com/doolong189" target="_blank">HoangLong</a></p>
+                                      </div>
+                                  </div>
+                              </body>
+                          </html>`,
 
             auth : {
                 user         : user_name,
