e1b790bfbf0fa58c893135eec1546f15f5716656
hoanglong <hoanglong180903@gmail.com>
2025-05-25 20:59:16 +0700

    Clean Coding 25-5-2025

--- src/models/Notification.js
+++ src/models/Notification.js
@@ -6,6 +6,7 @@ const NotificationSchema = mongoose.Schema({
     image: {type : String},
     idUser: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
     type : {type : Number}
-    //1: buy - 2: sell - 3: send message
+    //1: buy - 2: sell - 3: send message == ecommerce
+    //1: receiver message - 2: receiver book delivery - 3: done delivery - 4: cancel delivery
 });
 module.exports = mongoose.model('notification', NotificationSchema);
